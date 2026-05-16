67720a31be88b0491da021949359d657bb279940
Adam Camerer <ajc3xc@umsystem.edu>
2026-05-16 15:32:22 -0400

    chore: repo clean

--- last.bat
+++ last.bat
@@ -1,4 +1,4 @@
 cd /d C:\Users\13144\Documents\Meridian\repository
-del cleanup.bat 2>nul
+del cleanup2.bat 2>nul
 git add -A
-git commit -m "chore: final cleanup"
+git commit -m "chore: repo clean"
