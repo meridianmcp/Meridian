12e7688bfb3a76ae3657d3c2eca6825259f76c86
Adam Camerer <ajc3xc@umsystem.edu>
2026-05-16 15:32:12 -0400

    chore: remove temp build scripts

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+cd /d C:\Users\13144\Documents\Meridian\repository
+del do_commit.bat 2>nul
+git add -A
+git commit -m "chore: remove temp build scripts"
--- do_commit.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\Users\13144\Documents\Meridian\repository
-git add -A
-git commit -m "feat: v0.6.3 GOAL.md bidirectional file sync"
