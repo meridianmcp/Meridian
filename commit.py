8d0081c0b46fd900f171454a1f2a7998907f008d
Adam Camerer <ajc3xc@umsystem.edu>
2026-05-16 15:33:11 -0400

    chore: final repo clean

--- commit.py
+++ commit.py
@@ -1,5 +1,4 @@
 import subprocess, os
 os.chdir(r'C:\Users\13144\Documents\Meridian\repository')
-r = subprocess.run(['git', 'add', '-A'])
-r = subprocess.run(['git', 'commit', '-m', 'chore: gitignore bat files, repo clean'])
-print('done', r.returncode)
+subprocess.run(['git', 'add', '-A'])
+subprocess.run(['git', 'commit', '-m', 'chore: final repo clean'])
