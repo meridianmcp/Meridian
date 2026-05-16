f9d6218cab3d1850c88f8f9f92c66d1e83dd8980
Adam Camerer <ajc3xc@umsystem.edu>
2026-05-16 15:33:27 -0400

    chore: clean repo no temp files

--- commit.py
+++
@@ -1,4 +0,0 @@
-import subprocess, os
-os.chdir(r'C:\Users\13144\Documents\Meridian\repository')
-subprocess.run(['git', 'add', '-A'])
-subprocess.run(['git', 'commit', '-m', 'chore: final repo clean'])
